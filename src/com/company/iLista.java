1ff67a1411c61095f585f896892fc8552a816221
elmerhomero53 <elmerhomero53@gmail.com>
2020-02-23 10:28:19 -0700

    crear proyecto

---
+++ src/com/company/iLista.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public interface iLista<E> {
+    public int size();
+
+    public boolean isEmpty();
+
+    public void addFirst(E value);
+
+    public void addLast(E value);
+
+    public E getFirst();
+
+    public E removeFirst();
+
+    public E removeLast();
+
+    public boolean contains(E value);
+
+    public int indexOf(E value);
+
+    public E peek();
+}
