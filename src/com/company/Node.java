2b01e6904dd112eacb4728247c66158b0fac60db
elmerhomero53 <elmerhomero53@gmail.com>
2020-02-23 16:25:17 -0700

    agregar clases

---
+++ src/com/company/Node.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Node<E> {
+    private E data; // value stored in this element
+    protected Node<E> nextElement; // ref to next
+
+    public Node(E v, Node<E> next)   {
+        data = v;
+        nextElement = next;
+    }
+
+    public Node(E v)   {
+        this(v,null);
+    }
+
+    public Node<E> next()   {
+        return nextElement;
+    }
+
+    public void setNext(Node<E> next)   {
+        nextElement = next;
+    }
+
+    public E value()   {
+        return data;
+    }
+
+    public void setValue(E value)   {
+        data = value;
+    }
+}
